d9b1462937a2ca0108f14300ecddb6200d210428
Ameet Jadhav <ammugs11@gmail.com>
2022-09-02 11:34:29 +0530

    added Factor of a Number

---
+++ src/com/bridgelabzday9/FactorofaNumber.java
@@ -0,0 +1,20 @@
+package com.bridgelabzday9;
+
+public class FactorofaNumber {
+	
+	 public static void main(String[] args) {
+
+	      int num = 8;
+
+	     System.out.println( "Factors of " + num + " are " );
+
+	     // finding and printing factors b/w 1 to num
+	     for(int i = 1; i <= num; i++)
+	     {
+	         if(num % i == 0)
+	             System.out.println(i + " "); 
+	     }
+
+	      }
+
+}
